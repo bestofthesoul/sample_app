044d18c7af58df4205a58ab1bb2f6e3c85609d0a
sy s <sys@sys-MacBook-Air.local>
2015-08-19 12:40:38 +0800

    model user changed valid regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	# before_save { self.email = email.downcase }   ->> this works
+
+	before_save {email.downcase!} #--> this also works
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 255}, 
-				format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, 
+				format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}, 
 				uniqueness: { case_sensitive: false }
 
 	has_secure_password
