afc6bc6328208c1c0c34774b1fbe3f99eb4dfee4
sy s <sys@sys-MacBook-Air.local>
2015-08-23 16:08:06 +0800

    finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)# Log the user in and redirect to the user's show page.
+      log_in user# Log the user in and redirect to the user's show page.
       params[:session][:remember_me] == '1' ? remember(user) : forget(user) #the checkbox to choose remeber me ornot
       redirect_to user
     else
