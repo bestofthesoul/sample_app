351d212dd67ef6bfcda9fba78effcfb1ae79fe92
sy s <sys@sys-MacBook-Air.local>
2015-08-23 23:56:38 +0800

    account activation password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,12 @@ before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
+     # @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    # redirect_to root_url and return unless FILL_IN
   end
 
   def new
