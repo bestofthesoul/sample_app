311eca2a897550483232eba0a00673bbb3fda58e
sy s <sys@sys-MacBook-Air.local>
2015-08-23 22:59:36 +0800

    account activation and password reset

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -78,7 +78,7 @@ def setup
   end
   
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 
 
